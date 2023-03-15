87565c4b4473306d9d44caea823dae1f7cc05edd
Madhurima11 <37011183+Madhurima11@users.noreply.github.com>
2023-03-15 13:06:05 +0530

    Delete Our-Algorithm/n-10 directory

--- Our-Algorithm/n-10/field_now.m
+++
@@ -1,8 +0,0 @@
-// May be replaced by any field of choice
-load "all-fields/field-1-10.m";
-printf "\n defining poly=%o",f;
-K<y>:=NumberField(f); //THE NUMBER FIELD
-printf "\n will do ring of integers";
-time ok:=RingOfIntegers(K);
-ZZ:=IntegerRing();
-
